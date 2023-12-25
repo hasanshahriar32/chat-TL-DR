18888f6c8ab95641fa03082fe023807e7b10b794
MR. HASAN <hasanshahriar32@gmail.com>
2023-12-25 18:44:18 +0600

    Update home.tsx

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import { GetServerSideProps } from 'next';
